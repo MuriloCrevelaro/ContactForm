be25134a9dd1c95be396a4c95a06e2fa04dfe33b
MURILO CREVELARO DE CAMARGO <43376666806@info.local>
2026-04-16 16:16:45 -0300

    Cadastrar as contas

--- js/script.js
+++ js/script.js
@@ -1,18 +1,85 @@
+//Essa função recebe os dados do formulario em um objeto JavaScript
+async function cadastrarContato(objetoContato){
+    console.log(objetoContato)
+
+
+    //chamar a api com  fetch
+    const resposta = await fetch('http://localhost:3000/contatos', {
+        method: 'POST',
+        //Converte o objeto JavaCript para JSON, sendo string para o json asceitar
+        body: JSON.stringify(objetoContato) ,
+        Headers:{//informa para a api que o body está sendo enviado no formato json
+            "Content-Type": "application/json; charset-UFT-8"
+        }
+    })
+    return await resposta
+}
+
+
+
+
+async function buscarEndereco(cep){
+
+    //é fazer o que estavamos fazendo no navegador só que aqui
+    // let nome = "Murilo"
+    // let sobrenome = "Crevelaro"
+    // console.log(`O nome completo é: ${nome} ${sobrenome}`)
+
+    // quando o cep não vier preenchido
+    if(cep.trim().length < 8){
+        alert("O CEP tem que ter 8 digitos");
+        // // faz com que nenhuma função abaixo dela funcione
+        // return false;
+
+        //buscar o CEP lá na ViaCEP
+        try{
+            aguardandoCampo()
+
+            //Faz esperar um tempo para parecer que o pegar CEP demorou 2 segundos
+            setTimeout(async() => {
+                        // o await é para esperar a linha de funcionar e ai ela vai, só funciona com o async
+                let retorno = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+                let dados = await retorno.json()
+                console.log(dados)// objeto dados
+                
+                document.getElementById("logradouro").value = dados.logradouro
+                document.getElementById("regiao").value = dados.regiao
+                document.getElementById("complemento").value = dados.complemento
+                document.getElementById("bairro").value = dados.bairro
+                document.getElementById("cidade").value = dados.localidade
+                                                        //O "uf" faz vir somente a sigla do estado
+                document.getElementById("estado").value = dados.uf
+            },2000)
+        } catch (error){
+            console.log(error)
+        }
+    }
+}
+
+function aguardandoCampo(){
+    document.getElementById("logradouro").value = "Aguarde ..."
+    document.getElementById("regiao").value = "Aguarde ..."
+    document.getElementById("complemento").value = "Aguarde ..."
+    document.getElementById("bairro").value = "Aguarde ..."
+    document.getElementById("cidade").value = "Aguarde ..."
+    document.getElementById("estado").value = "Aguarde ..."
+}
+
 function validarFormulario(){
-    let nome = document.getElementById("nome").value;
-    let sobrenome = document.getElementById("nome").value;
-    let email = document.getElementById("nome").value;
-    let pais = document.getElementById("nome").value;
-    let ddd = document.getElementById("nome").value;
-    let numero = document.getElementById("nome").value;
-    let cep = document.getElementById("nome").value;
-    let Rua = document.getElementById("nome").value;
-    let numero__casa = document.getElementById("nome").value;
-    let bloco = document.getElementById("nome").value;
-    let bairro = document.getElementById("nome").value;
-    let cidade = document.getElementById("nome").value;
-    let estado = document.getElementById("nome").value;
-    let anotação = document.getElementById("nome").value;
+     let nome = document.getElementById("nome").value;
+     let sobrenome = document.getElementById("sobrenome").value;
+    let email = document.getElementById("email").value;
+     let pais = document.getElementById("pais").value;
+     let ddd = document.getElementById("ddd").value;
+     let numero = document.getElementById("numero").value;
+     let cep = document.getElementById("cep").value;
+     let logradouro = document.getElementById("logradouro").value;
+     let regiao = document.getElementById("regiao").value;
+      let complemento = document.getElementById("complemento").value;
+     let bairro = document.getElementById("bairro").value;
+     let cidade = document.getElementById("cidade").value;
+     let estado = document.getElementById("estado").value;
+     let anotação = document.getElementById("anotação").value;
     let quantidadeErros = 0;
 
     if(nome.trim().length == 0){
@@ -23,12 +90,112 @@ function validarFormulario(){
         reiciaBorda("nome")
     }
 
+    if(sobrenome.trim().length == 0){
+        formError("sobrenome");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("sobrenome")
+    }
+
+    if(email.trim().length == 0){
+        formError("email");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("email")
+    }
+    if(pais.trim().length == 0){
+        formError("pais");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("pais")
+    }
+    if(ddd.trim().length == 0){
+        formError("ddd");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("ddd")
+    }
+    if(numero.trim().length == 0){
+        formError("numero");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("numero")
+    }
+    if(cep.trim().length == 0){
+        formError("cep");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("cep")
+    }
+    if(logradouro.trim().length == 0){
+        formError("logradouro");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("logradouro")
+    }
+    if(regiao.trim().length == 0){
+        formError("regiao");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("regiao")
+    }
+    if(complemento.trim().length == 0){
+        formError("complemento");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("complemento")
+    }
+    if(bairro.trim().length == 0){
+        formError("bairro");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("bairro")
+    }
+    if(cidade.trim().length == 0){
+        formError("cidade");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("cidade")
+    }
+    if(estado.trim().length == 0){
+        formError("estado");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("estado")
+    }
+    if(anotação.trim().length == 0){
+        formError("anotação");
+        quantidadeErros++;
+    }else{
+        reiciaBorda("anotaçãos")
+    }
+
     if(quantidadeErros > 0){
         alert("Existem " + quantidadeErros + " erros no formulario");
+        //reinicia a contagem
         quantidadeErros = 0;
     }else {
-        alert("Formulario enviado com sucesso")
-        reiniciaTodasAsBordas("nome");
+
+        let objetoContato = {
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email,
+            pais: pais,
+            ddd: ddd,
+            numero: numero,
+            cep: cep,
+            logradouro: logradouro,
+            regiao: rigiao,
+            complemento: complemento,
+            bairro: bairro,
+            cidade: cidade,
+            estado: estado,
+            anotação: anotação
+        }
+
+        // alert("Formulario enviado com sucesso")
+        let cadastrar = cadastrarContato(objetoContato)
+        // reiniciaTodasAsBordas();
     }
 }
 
@@ -37,14 +204,23 @@ function formError(idCampo){
     document.getElementById(idCampo).style.border="2px solid red";
 }
 
-// function reiciaBorda(idCampo){
-//     document.getElementById(idCampo).style.border = "2px solid transparent";
-// }
+function reiciaBorda(idCampo){
+    document.getElementById(idCampo).style.border = "2px solid transparent";
+}
 
-function reiniciaTodasAsBordas(){
-    const campos = ["nome", "email", "telefone", "cep"]; // adiciona todos
+function reiniciaTodasAsBarras(){
+    const campos = document.querySelectorAll("input, textarea");
 
-    campos.forEach(id => {
-        document.getElementById(id).style.border = "2px solid transparent";
+    campos.forEach(campo => {
+        campo.style.input = "none";
     });
 }
+
+//Exemplo de como limpar todas as bordas de uma vez
+// function reiniciaTodasAsBordas(){
+//     const campos = document.querySelectorAll("input, textarea");
+
+//     campos.forEach(campo => {
+//         campo.style.border = "2px solid transparent";
+//     });
+// }
